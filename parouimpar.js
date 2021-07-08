a03d7aed68424a5a94e5893c2cd9f4bf16ca3c25
Jonathan D Maciel Ribeiro <jonathandadaltmaciel@gmail.com>
2021-07-08 00:57:43 -0300

    Add files via upload

---
+++ parouimpar.js
@@ -0,0 +1,6 @@
+let numero = 8;
+
+// % = dividido
+
+let paridade = numero % 2 == 0 ? 'par' : 'impar';
+console.log(paridade);
---
+++ switch-case.js.js
@@ -0,0 +1,19 @@
+let sinal = 'amasdsarelo';
+
+switch(sinal){
+
+case 'verde':
+    console.log('pode passar');
+break;
+
+case 'amarelo':
+    console.log ('atenção, prestes a fechar');
+break;
+
+case 'vermelho':
+    console.log('Fechado, pare!');
+break;
+
+default:
+    console.log('Sinal Inválido')
+}
