31dedf596af561ff14810e8780e2341423e88a51
Jonathan D Maciel Ribeiro <jonathandadaltmaciel@gmail.com>
2021-07-08 00:59:32 -0300

    Add files via upload

---
+++ if-else.js.js
@@ -0,0 +1,16 @@
+let numero =2;
+
+if(numero > 0){
+    console.log('O número é positivo!');
+}
+
+else 
+
+if(numero == 0){
+    console.log('numero é zero!');
+}
+
+
+else{
+    console('O numero é negativo!');
+}
